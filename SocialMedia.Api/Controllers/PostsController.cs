eccf455181711e82a5346470b64b56b5482cbec2
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2023-06-03 21:19:35 +0500

    Initial Architecture

---
+++ SocialMedia.Api/Controllers/PostsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Domain.Models;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var post = new Post
+            {
+                Id = id,
+                Text = "Hello World!",
+            };
+
+            return Ok(post);
+        }
+    }
+}
