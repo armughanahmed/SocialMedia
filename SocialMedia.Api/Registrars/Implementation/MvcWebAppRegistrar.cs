4d51a68ac687e2aca8db41a0ac000ecca190f4e1
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2023-06-05 04:37:15 +0500

    removed SwaggerWebAppRegistrar File
    - moved swagger middleware to mvc middleware

--- SocialMedia.Api/Registrars/Implementation/MvcWebAppRegistrar.cs
+++ SocialMedia.Api/Registrars/Implementation/MvcWebAppRegistrar.cs
@@ -1,5 +1,6 @@
 ﻿namespace SocialMedia.Api.Registrars.Implementation
 {
+    using Microsoft.AspNetCore.Mvc.ApiExplorer;
     using SocialMedia.Api.Registrars.Interface;
 
     /// <summary>
@@ -13,6 +14,17 @@
         /// <param name="app"><see cref="WebApplication"/></param>
         public void RegisterPipelineComponents(WebApplication app)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.ApiVersion.ToString());
+                }
+            });
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
