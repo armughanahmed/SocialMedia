23ae665c49c4bd70b486b10b2919b68aba305252
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2023-06-10 04:15:25 +0500

    Identity User Token Configuration

---
+++ Dal/Configurations/IdentityUserTokenConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace SocialMedia.Dal.Configurations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    /// <summary>
+    /// Identity User Token Configurations
+    /// </summary>
+    internal class IdentityUserTokenConfig : IEntityTypeConfiguration<IdentityUserToken<string>>
+    {
+        /// <inheritdoc/>
+        public void Configure(EntityTypeBuilder<IdentityUserToken<string>> builder)
+        {
+            builder.HasNoKey();
+        }
+    }
+}
