9ae0a93595fec6b6212efae289a6a1d3d6ae2c76
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2023-06-10 03:01:45 +0500

    DbContext Added
    - Added DbContext file and DbSet

---
+++ Dal/DbContext.cs
@@ -0,0 +1,43 @@
+﻿namespace SocialMedia.Dal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using SocialMedia.Domain.Aggregates.PostAggregate;
+    using SocialMedia.Domain.Aggregates.UserProfileAggregate;
+
+    /// <summary>
+    /// Database Context Class
+    /// </summary>
+    public class DbContext : IdentityDbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DbContext"/> class for using options.
+        /// </summary>
+        /// <param name="options">dbcontext options</param>
+        public DbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets UserProfile DbContext
+        /// </summary>
+        public DbSet<UserProfile>? UserProfiles { get; set; }
+
+        /// <summary>
+        /// Gets or sets Posts DbContext
+        /// </summary>
+        public DbSet<Post>? Posts { get; set; }
+
+        /// <inheritdoc/>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Method intentionally left empty.
+        }
+    }
+}
