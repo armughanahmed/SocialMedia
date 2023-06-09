f540c78957606b87c3b3130807b85a9af8dbf392
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2023-06-10 04:15:13 +0500

    Identity User Role Configuration

---
+++ Dal/Configurations/IdentityUserRoleConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace SocialMedia.Dal.Configurations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    /// <summary>
+    /// Identity User Role Configurations
+    /// </summary>
+    internal class IdentityUserRoleConfig : IEntityTypeConfiguration<IdentityUserRole<string>>
+    {
+        /// <inheritdoc/>
+        public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+        {
+            builder.HasKey(x => x.RoleId);
+        }
+    }
+}
