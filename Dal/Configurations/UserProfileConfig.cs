e642bb72f4aad86e25af8f37aef5bcfab06c8ca9
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2023-06-10 04:14:30 +0500

    User Profile Configuration

---
+++ Dal/Configurations/UserProfileConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace SocialMedia.Dal.Configurations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using SocialMedia.Domain.Aggregates.PostAggregate;
+    using SocialMedia.Domain.Aggregates.UserProfileAggregate;
+
+    /// <summary>
+    /// User Profile Configurations
+    /// </summary>
+    internal class UserProfileConfig : IEntityTypeConfiguration<UserProfile>
+    {
+        /// <inheritdoc/>
+        public void Configure(EntityTypeBuilder<UserProfile> builder)
+        {
+            builder.HasKey(x => x.BasicInfo);
+        }
+    }
+}
