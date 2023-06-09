f892a70e004df55c0422aa7c2671f13b75d80f72
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2023-06-10 04:14:01 +0500

    Post Comment Configurations

---
+++ Dal/Configurations/PostCommentConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace SocialMedia.Dal.Configurations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using SocialMedia.Domain.Aggregates.PostAggregate;
+
+    /// <summary>
+    /// Post Comments Configurations
+    /// </summary>
+    internal class PostCommentConfig : IEntityTypeConfiguration<PostComment>
+    {
+        /// <inheritdoc/>
+        public void Configure(EntityTypeBuilder<PostComment> builder)
+        {
+            builder.HasKey(x => x.CommentId);
+        }
+    }
+}
