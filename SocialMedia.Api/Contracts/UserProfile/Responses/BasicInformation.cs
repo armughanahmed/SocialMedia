7b8980fd2f88242906b2938c1f4d0dacb2ae8653
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2023-06-12 05:17:40 +0500

    API Contracts Added
    - added api requests and responses

---
+++ SocialMedia.Api/Contracts/UserProfile/Responses/BasicInformation.cs
@@ -0,0 +1,38 @@
+﻿namespace SocialMedia.Api.Contracts.UserProfile.Responses
+{
+    /// <summary>
+    /// Basic Information record contains basic information about the user that needs to be sent in response contract
+    /// </summary>
+    public record BasicInformation
+    {
+        /// <summary>
+        /// Gets or sets first Name
+        /// </summary>
+        public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets LastName
+        /// </summary>
+        public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets Email Address
+        /// </summary>
+        public string EmailAddress { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets Phone
+        /// </summary>
+        public string Phone { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets Date Of Birth
+        /// </summary>
+        public DateTime? DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Gets or sets Current City
+        /// </summary>
+        public string CurrentCity { get; set; } = string.Empty;
+    }
+}
