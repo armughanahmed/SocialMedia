3dbd8ed6be1a7bd65c59b300eb0844d13b819169
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2023-06-12 05:16:49 +0500

    Delete user profile command and handler
    - added delete user profile command and it's handler

---
+++ SocialMedia.Application/UserProfiles/CommandHandlers/DeleteUserProfileHandler.cs
@@ -0,0 +1,46 @@
+﻿namespace SocialMedia.Application.UserProfiles.CommandHandlers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using MediatR;
+    using Microsoft.EntityFrameworkCore;
+    using SocialMedia.Application.UserProfile.Commands;
+    using SocialMedia.Application.UserProfiles.Commands;
+    using SocialMedia.Dal;
+    using SocialMedia.Domain.Aggregates.UserProfileAggregate;
+
+    /// <summary>
+    /// Update User Profile Handler
+    /// </summary>
+    public class DeleteUserProfileHandler : IRequestHandler<DeleteUserProfile, Unit>
+    {
+        /// <summary>
+        /// SocialMedia Db Context
+        /// </summary>
+        private readonly SocialMediaContext _dbContext;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeleteUserProfileHandler"/> class.
+        /// </summary>
+        /// <param name="dbContext">SocialMedia Database Context</param>
+        public DeleteUserProfileHandler(SocialMediaContext dbContext) => _dbContext = dbContext;
+
+        /// <inheritdoc/>
+        public async Task<Unit> Handle(DeleteUserProfile request, CancellationToken cancellationToken)
+        {
+            var userProfile = await _dbContext.UserProfiles.FirstOrDefaultAsync(x => x.UserProfileId == request.UserProfileId);
+
+            if(userProfile != null)
+            {
+                _dbContext.UserProfiles.Remove(userProfile);
+                await _dbContext.SaveChangesAsync();
+            }
+
+            return default(Unit);
+        }
+    }
+}
---
+++ SocialMedia.Application/UserProfiles/Commands/DeleteUserProfile.cs
@@ -0,0 +1,20 @@
+﻿namespace SocialMedia.Application.UserProfiles.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using MediatR;
+
+    /// <summary>
+    /// Delete User Profile Command
+    /// </summary>
+    public class DeleteUserProfile : IRequest<Unit>
+    {
+        /// <summary>
+        /// Gets or sets User Profile Id
+        /// </summary>
+        public Guid UserProfileId { get; set; }
+    }
+}
