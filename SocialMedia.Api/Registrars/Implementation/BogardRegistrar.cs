a919e1c741c16226eef994c7e2db25ae7d5ae21f
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2023-06-12 05:11:48 +0500

    Register Services for Bogard
    - AutoMapper services added
    - MediatR services added

---
+++ SocialMedia.Api/Registrars/Implementation/BogardRegistrar.cs
@@ -0,0 +1,21 @@
+﻿namespace SocialMedia.Api.Registrars.Implementation
+{
+    using SocialMedia.Api.Registrars.Interface;
+    using SocialMedia.Application.UserProfiles.Queries;
+
+    /// <summary>
+    /// Register Services Made by Bogard (AutoMapper, MediatR)
+    /// </summary>
+    public class BogardRegistrar : IWebApplicationBuilderRegistrar
+    {
+        /// <inheritdoc/>
+        public void RegisterServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddAutoMapper(typeof(Program), typeof(GetAllUserProfiles));
+            builder.Services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssemblyContaining<GetAllUserProfiles>();
+            });
+        }
+    }
+}
