2a945fdda85e4b2e425c0169ffd574a457d5e0ef
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2023-06-12 05:16:01 +0500

    update user profile command and handler
    - added command and handler for update user profile

---
+++ SocialMedia.Application/UserProfiles/CommandHandlers/UpdateUserProfileBasicInfoHandler.cs
@@ -0,0 +1,50 @@
+﻿namespace SocialMedia.Application.UserProfiles.CommandHandlers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using MediatR;
+    using Microsoft.EntityFrameworkCore;
+    using SocialMedia.Application.UserProfile.Commands;
+    using SocialMedia.Application.UserProfiles.Commands;
+    using SocialMedia.Dal;
+    using SocialMedia.Domain.Aggregates.UserProfileAggregate;
+
+    /// <summary>
+    /// Update User Profile Handler
+    /// </summary>
+    public class UpdateUserProfileBasicInfoHandler : IRequestHandler<UpdateUserProfileBasicInfo, Unit>
+    {
+        /// <summary>
+        /// SocialMedia Db Context
+        /// </summary>
+        private readonly SocialMediaContext _dbContext;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateUserProfileBasicInfoHandler"/> class.
+        /// </summary>
+        /// <param name="dbContext">SocialMedia Database Context</param>
+        public UpdateUserProfileBasicInfoHandler(SocialMediaContext dbContext) => _dbContext = dbContext;
+
+        /// <inheritdoc/>
+        public async Task<Unit> Handle(UpdateUserProfileBasicInfo request, CancellationToken cancellationToken)
+        {
+            var userProfile = await _dbContext.UserProfiles.FirstOrDefaultAsync(x => x.UserProfileId == request.UserProfileId);
+
+            var basicInfo = BasicInfo.CreateBasicInfo(request.FirstName, request.LastName, request.EmailAddress, request.Phone, request.DateOfBirth, request.CurrentCity);
+
+            if(userProfile != null)
+            {
+                userProfile.UpdateBasicInfo(basicInfo);
+
+                _dbContext.UserProfiles.Update(userProfile);
+                await _dbContext.SaveChangesAsync();
+            }
+
+            return default(Unit);
+        }
+    }
+}
---
+++ SocialMedia.Application/UserProfiles/Commands/UpdateUserProfileBasicInfo.cs
@@ -0,0 +1,51 @@
+﻿namespace SocialMedia.Application.UserProfiles.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using MediatR;
+    using SocialMedia.Domain.Aggregates.UserProfileAggregate;
+
+    /// <summary>
+    /// Update User Profile Basic Info Command
+    /// </summary>
+    public class UpdateUserProfileBasicInfo : IRequest<Unit>
+    {
+        /// <summary>
+        /// Gets or Sets User Profile Id
+        /// </summary>
+        public Guid UserProfileId { get; set; }
+
+        /// <summary>
+        /// Gets or Sets First Name
+        /// </summary>
+        public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or Sets LastName
+        /// </summary>
+        public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or Sets Email Address
+        /// </summary>
+        public string EmailAddress { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or Sets Phone
+        /// </summary>
+        public string Phone { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or Sets Date Of Birth
+        /// </summary>
+        public DateTime? DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Current City
+        /// </summary>
+        public string CurrentCity { get; set; } = string.Empty;
+    }
+}
